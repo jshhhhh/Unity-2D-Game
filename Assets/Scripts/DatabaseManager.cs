817e62d4ebd0fde0734410402a2d6bfc4ce50cef
jshhhhh <show959@hanmail.net>
2021-12-05 21:56:32 +0900

    create item

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -13,12 +13,7 @@ public class DatabaseManager : MonoBehaviour
 
     static public DatabaseManager instance;
 
-    public string[] var_name;
-    public float[] var;
-
-    public string[] switch_name;
-    public bool[] switches;
-
+    #region Singleton
     private void Awake() {
         if(instance != null)
         {
@@ -31,10 +26,30 @@ public class DatabaseManager : MonoBehaviour
             instance = this;
         }
     }
+    #endregion Singleton
+
+    public string[] var_name;
+    public float[] var;
+
+    public string[] switch_name;
+    public bool[] switches;
+
+    //아이템 리스트 생성
+    public List<Item> itemList = new List<Item>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //아이템 정보?
+        itemList.Add(new Item(10001, "빨간 포션", "체력을 50 채워주는 기적의 물약", Item.ItemType.Use));
+        itemList.Add(new Item(10002, "파란 포션", "마나를 15 채워주는 기적의 물약", Item.ItemType.Use));
+        itemList.Add(new Item(10003, "농축 빨간 포션", "체력을 350 채워주는 기적의 농축 물약", Item.ItemType.Use));
+        itemList.Add(new Item(10004, "농축 파란 포션", "마나를 80 채워주는 기적의 농축 물약", Item.ItemType.Use));
+        itemList.Add(new Item(11001, "랜덤 상자", "랜덤으로 포션이 나온다. 낮은 확률로 꽝", Item.ItemType.Use));
+        itemList.Add(new Item(20001, "짧은 검", "기본적인 용사의 검", Item.ItemType.Equip));
+        itemList.Add(new Item(21001, "사파이어 반지", "1분에 마나 1을 회복시켜주는 마법 반지", Item.ItemType.Equip));
+        itemList.Add(new Item(30001, "고대 유물의 조각 1", "반으로 쪼개진 고대 유물의 파편", Item.ItemType.Quest));
+        itemList.Add(new Item(30002, "고대 유물의 조각 2", "반으로 쪼개진 고대 유물의 파편", Item.ItemType.Quest));
+        itemList.Add(new Item(30003, "고대 유물", "고대 유적에 잠들어있던 고대의 유물", Item.ItemType.Quest));
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    //아이템의 고유 ID값. 중복 불가능
+    public int itemID;
+    //아이템의 이름. 중복 가능
+    public string itemName;
+    //아이템 설명
+    public string itemDescription;
+    //소지 갯수
+    public int itemCount;
+    //아이템의 아이콘
+    public Sprite itemIcon;
+    //아이템타입
+    public ItemType itemType;
+
+    //타입 열거(이외의 값을 벗어난 값을 대입할 경우 오류)
+    public enum ItemType
+    {
+        Use,
+        Equip,
+        Quest,
+        ETC
+    }
+
+    //생성자(호출되는 순간 생성자를 통해 값을 채움)
+    public Item(int _itemID, string _itemName, string _itemDes, ItemType _itemType, int _itemCount = 1)
+    {
+        itemID = _itemID;
+        itemName = _itemName;
+        itemDescription = _itemDes;
+        itemType = _itemType;
+        itemCount = _itemCount;
+        //Sprite를 Resource의 해당 경로의 파일을 로드
+        //물음표 / 사용 필수(키패드 / 쓰면 오류 남)
+        //typeof(Sprite): Sprite로 가져옴(명시적 변환)
+        //as Sprite: 실제로 변환
+        itemIcon = Resources.Load("ItemIcon/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
